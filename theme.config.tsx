3449ffcda3a702f24b81304722473d49df19a85c
aliveornot <aliveornot@gmail.com>
2023-11-26 16:10:12 +0900

    second commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Vigma RN</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/aliveornot/vigma-doc',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  // docsRepositoryBase: 'https://github.com/aliveornot/vigma-doc',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Powered by Nextra',
   },
-}
+  editLink: {
+    text: 'Edit this page on GitHub',
+    component: ({ className, filePath }) => (
+      <a className={className} href={`https://github.com/aliveornot/vigma-doc/${filePath}`}>
+        edit this page on github
+      </a>
+    ),
+  },
+};
 
-export default config
+export default config;
