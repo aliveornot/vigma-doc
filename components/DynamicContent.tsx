2659b6934a1087d158b44b47db66fb82a85d0067
dongho <aliveornot@gmail.com>
2023-12-05 22:01:01 +0900

    temporary login for HN

---
+++ components/DynamicContent.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
+// This is a React component within your MDX file
+const DynamicContent = () => {
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const from = params.get('from');
+
+    if (from === 'hackerNews') {
+      setShowContent(true);
+    }
+  }, []);
+
+  if (!showContent) return null;
+  return (
+    <div style={{ color: 'red', fontWeight: 'bold' }}>
+      Hi hackers! You do NOT need to fill out a form. Use
+      <br />
+      <span style={{ color: 'yellow' }}>
+        &nbsp; email: hacker@news.com <br />
+        &nbsp; password: hackers
+      </span>{' '}
+      <br />
+      for login.
+      {showContent}
+    </div>
+  );
+};
+
+export default DynamicContent;
