6e4c1abdb7351225fd03f61348b1bdfe762c9f9c
aliveornot <aliveornot@gmail.com>
2023-12-17 15:50:48 +0900

    pixel

---
+++ pages/_app.tsx
@@ -0,0 +1,27 @@
+import App, { AppContext, AppInitialProps, AppProps } from 'next/app';
+import Head from 'next/head';
+
+type AppOwnProps = { example: string };
+
+export default function MyApp({ Component, pageProps, example }: AppProps & AppOwnProps) {
+  console.log(1);
+  return (
+    <>
+      <Head>
+        {/* X pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},
+            s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('init','YOUR_PIXEL_ID');
+            twq('track','PageView');
+            `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
