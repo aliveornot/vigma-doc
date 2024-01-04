8d324029e4cd43cf5c1f8c74fec72d0306227d92
aliveornot <aliveornot@gmail.com>
2024-01-04 15:13:22 +0900

    change pixel id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function MyApp({ Component, pageProps, example }: AppProps & AppO
             !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},
             s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
             a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-            twq('init','oi36s');
+            twq('init','oisfx');
             twq('track','PageView');
             `,
           }}
