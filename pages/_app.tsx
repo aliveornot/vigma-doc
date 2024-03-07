d62c731942c0f01cc9693c25ac3c26434f40efc8
aliveornot <aliveornot@gmail.com>
2024-03-07 16:12:35 +0900

    reddit pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,8 +14,15 @@ export default function MyApp({ Component, pageProps, example }: AppProps & AppO
             !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},
             s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
             a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-            twq('init','oisfx');
+            twq('init','oi36s');
             twq('track','PageView');
+            `,
+          }}
+        />
+        {/* reddit pixel */}
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_elzcxjtsr2up', {"aaid":"<AAID-HERE>","email":"<EMAIL-HERE>","externalId":"<EXTERNAL-ID-HERE>","idfa":"<IDFA-HERE>"});rdt('track', 'PageVisit');
             `,
           }}
         />
