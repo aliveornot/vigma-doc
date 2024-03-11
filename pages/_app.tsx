35145aabd9c751d45d4fd22f0f103396d8c0657f
aliveornot <aliveornot@gmail.com>
2024-03-11 19:12:36 +0900

    script ids

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,7 +32,7 @@ export default function MyApp({ Component, pageProps, example }: AppProps & AppO
         <Script
           type='text/javascript'
           dangerouslySetInnerHTML={{
-            __html: ` (function(c,l,a,r,i,t,y){
+            __html: `(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
