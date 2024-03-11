4ffe807565c7ec7d86c5dbbd4e1e15835b16bfd8
aliveornot <aliveornot@gmail.com>
2024-03-11 18:42:25 +0900

    clarity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,6 +26,16 @@ export default function MyApp({ Component, pageProps, example }: AppProps & AppO
             `,
           }}
         />
+        {/* clarity */}
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: ` (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "lexwqu5vr6");`,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
